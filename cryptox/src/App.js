3867a615ff615ea322e92f9f56e3d33e606ffedc
aramistamboulian961 <aramistamboulian961@gmail.com>
2022-10-07 16:17:32 +0300

    still have to fix an api problem in the code

---
+++ cryptox/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Coins from "./components/Coins";
+import Navbar from "./components/Navbar";
+import Coin from '../src/Routes/Coin'
+import { Routes, Route } from "react-router-dom";
+function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=150&page=1&sparkline=false'
+  const theData = async () => {
+    try {
+      const theResponse = await axios.get(url)
+      setCoins(theResponse.data)
+      console.log(theResponse.data[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    theData()
+
+    // axios
+    //   .get(url)
+    //   .then((response) => {
+    //     setCoins(response.data);
+    //     console.log(response.data[0]);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Coins coins={coins} />} />
+        <Route path="/coin" element={<Coin />}>
+          <Route path=":coinId" element={<Coin />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
